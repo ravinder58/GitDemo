a70fb2b1dfbf863969f4127ecf1c8c63ef1d8164
ravinder <singhravinder868@gmail.com>
2019-04-25 09:12:29 +1000

    American time commited

--- src/test/java/TestCases/RestAPITest.java
+++ src/test/java/TestCases/RestAPITest.java
@@ -7,7 +7,11 @@ public class RestAPITest {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
-		
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
 	}
 	@Test
 	public void deleteTwitter()
